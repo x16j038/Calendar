b4eabcc483f31949ef95508fd64e3ca9fa254ffe
x16j038 <x16j038@chiba-fjb.ac.jp>
2017-04-26 12:13:33 +0900

    プログラム作成中

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@ namespace Calendar
     {
         static void Main(string[] args)
         {
+            DateTime dateNow = DateTime.Now;
+            Console.Write("{0}年 {1}月\n", dateNow.Year, dateNow.Month);
+            //Console.ReadLine();
         }
     }
 }
